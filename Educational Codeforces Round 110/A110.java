309155a2e9326c6d594087507c41b33ad1673e24
gaoyunfeng <gaoyunfeng1995@gmail.com>
2021-06-04 22:41:29 +0800

    A. Fair Playoff 水题

---
+++ Educational Codeforces Round 110/A110.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class A110 {
+
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Solver solver = new Solver();
+        solver.solve(in, out);
+        out.close();
+    }
+
+    static class Solver {
+        void solve(InputReader in, PrintWriter out) {
+            int t = in.nextInt();
+            while (t-- > 0) {
+                int[] a = new int[4];
+                for (int i = 0; i < 4; i++) {
+                    a[i] = in.nextInt();
+                }
+                int max12 = Math.max(a[0], a[1]);
+                int max34 = Math.max(a[2], a[3]);
+                int min12 = Math.min(a[0], a[1]);
+                int min34 = Math.min(a[2], a[3]);
+                out.println((max12 > min34) && (max34 > min12) ? "YES" : "NO");
+            }
+        }
+    }
+
+    static class InputReader {
+        BufferedReader reader;
+        StringTokenizer tokenizer;
+
+        InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
