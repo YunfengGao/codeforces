e5313bba379b185066c325fd35eb800b8968b05f
gaoyunfeng <gaoyunfeng1995@gmail.com>
2021-06-05 18:49:06 +0800

    B. Array Reodering 贪心+暴力

--- Educational Codeforces Round 110/B1535.java
+++ Educational Codeforces Round 110/B1535.java
@@ -7,7 +7,7 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class B110 {
+public class B1535 {
 
     public static void main(String[] args) {
         InputStream inputStream = System.in;
